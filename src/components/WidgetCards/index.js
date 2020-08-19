50a1fed93d1a875dc4e19e9aab3b74278a4e6b50
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-19 22:47:30 +0300

    update WidgetCards

--- src/components/WidgetCards/index.js
+++ src/components/WidgetCards/index.js
@@ -2,62 +2,32 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 import breakpoint from 'styled-components-breakpoint'
 import { Container, Grid, CardShip } from 'components'
-// import { h2 } from 'base/mixins/text'
 
 const WidgetCards = () => {
     const [ ships, setShips ] = useState()
 
-
-    // useEffect(()=> {
-    //     fetch("https://swapi.dev/api/films/2")
-    //         .then(res => res.json())
-    //         .then(film => {
-    //             const starships = film.starships //Ссылки на данные конкретных
-    //             const array = []
-    //             starships.forEach(ship => {
-    //                 fetch(ship)
-    //                     .then(res => res.json())
-    //                     .then(data => { 
-    //                         array[array.length] = data
-    //                     })
-    //             })
-    //             console.log(array)
-    //             setShips(array) 
-    //         })          
-    // }, [])
-
-    useEffect(async () => {  //еще одна попытка
-        let response = await fetch('https://swapi.dev/api/films/2');
-        let data = await response.json();
-        
-        const starships = data.starships
-        const array = []
-
-        function fullArray(array) {
-            starships.forEach(async(ship) => {
-                let response = await fetch(ship);
-                let data = await response.json();
-                array.push(data)
+    useEffect(()=> {
+        fetch("https://swapi.dev/api/films/2").then(res => res.json())
+            .then(film => {
+                const starships = film.starships //Ссылки на данные конкретных объектов
+                return Promise.all(starships.map(ship => fetch(ship).then(res => res.json())))
             })
-            return array
-        }
-        let result = await fullArray(array)
-        console.log(result)
-
-      }, []);
-      
+            .then(data => 
+                setShips(data)
+            )         
+    }, [])
+    
     if(!ships) {
         return 'Loading...'
     }
     return (
         <Wrapper>
             <Container>
-            <h1>ебана</h1>
                 <Grid >
                     {
                         ships.map((ship) => {
                             return (
-                                <Card key={ship}>
+                                <Card key={ship.created}>
                                     <CardShip ship={ship}/>
                                 </Card>
                             )
@@ -79,20 +49,5 @@ const Card = styled.div`
         grid-column:  2 / span 10;
     `}
 `
-// const Input = styled.input`
-//     ${h2};
-//     padding: 10px;
-//     grid-row: 1;
-//     grid-column: 4 / span 6;
-//     border-radius: 10px;
-//     border: 1px solid ${p => p.theme.color.primary};
-//     color: ${p => p.theme.color.black};
-//     &:hover, &:focus, &:active {
-//         outline: none;
-//     }
-//     ${breakpoint('xs', 'sm')`
-//         grid-column:  2 / span 10;
-//     `}
-// `
 
 export default WidgetCards
