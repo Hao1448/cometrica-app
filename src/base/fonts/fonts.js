ffdce9bee4814a7238d577bc3ba3709df9917ac5
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-21 02:08:45 +0300

    Create fonts.js

---
+++ src/base/fonts/fonts.js
@@ -0,0 +1,22 @@
+export const GlobalFonts = `
+    @font-face {
+        font-family: 'Roboto';
+        src: url('./Roboto/Roboto.eot');
+        src: url('./Roboto/Roboto.eot?#iefix') format('embedded-opentype'),
+            url('./Roboto/Roboto.woff2') format('woff2'),
+            url('./Roboto/Roboto.woff') format('woff'),
+            url('./Roboto/Roboto.ttf') format('truetype'),
+            url('./Roboto/Roboto.svg#Roboto') format('svg');
+        font-weight: normal;
+    }  
+    @font-face {
+        font-family: 'Roboto';
+        src: url('./Roboto/Roboto-Bold.eot');
+        src: url('./Roboto/Roboto-Bold.eot?#iefix') format('embedded-opentype'),
+            url('./Roboto/Roboto-Bold.woff2') format('woff2'),
+            url('./Roboto/Roboto-Bold.woff') format('woff'),
+            url('./Roboto/Roboto-Bold.ttf') format('truetype'),
+            url('./Roboto/Roboto-Bold.svg#Roboto-Bold') format('svg');
+        font-weight: 700;
+    }
+`
