aa2d5d4d37a9e81f2ec1ead89b1edd5e24eed159
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-21 02:09:02 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import Container from './Container'
 import CardShip from './CardShip'
 import WidgetCards from './WidgetCards'
 import DropDown from './DropDown'
+import UiIcon from './UiIcon'
 
 
-export { Grid, Container, CardShip, WidgetCards, DropDown }
+export { Grid, Container, CardShip, WidgetCards, DropDown, UiIcon }
