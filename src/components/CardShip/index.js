5f682b3c21b527ac866d346df8f7eba0b8823f68
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-20 19:46:53 +0300

    Update CardShip

--- src/components/CardShip/index.js
+++ src/components/CardShip/index.js
@@ -2,17 +2,86 @@ import React from 'react';
 import styled from 'styled-components'
 import { h2, p } from 'base/mixins/text'
 
-const CardShip = ({ship}) => {
+// const compare = (ship, mainShip, field) => {
+//     const shipValue = Math.round(ship[field].replace(',','') * 100);
+//     const mainShipValue = Math.round(mainShip[field].replace(',','') * 100);
+//     let delta = null;
+//     if(!Number.isNaN(shipValue) && !Number.isNaN(mainShipValue)) {
+//         delta = shipValue - mainShipValue;
+//         if(delta === 0) {
+//             return '';
+//         }
+//         if(delta > 0) {
+//             return (
+//                 <span style={{color:'green'}}>
+//                     {`(+${delta/100})`}
+//                 </span>
+//             )
+//         }
+//         if(delta < 0) {
+//             return (
+//                 <span style={{color:'red'}}>
+//                     {`(${delta/100})`}
+//                 </span>
+//             )
+//         }
+//     } else {
+//         return '';
+//     }
+// }
+
+const CardShip = ({ship, mode, mainShip}) => {
+
+    const secondaryFlag = (mode === 'secondary')
+
+    const fields = {
+        cargo_capacity: 'Грузоподъемность',
+        cost_in_credits: 'Стоимость',
+        crew: 'Экипаж',
+        hyperdrive_rating: 'Рейтинг гипердвигателя',
+        length: 'Длина',
+        max_atmosphering_speed: 'Скорость в атмосфере',
+        passengers: 'Пассажиры'
+    }
+
+    const compare = (ship, mainShip, field) => {
+        const shipValue = Math.round(ship[field].replace(',','') * 100);
+        const mainShipValue = Math.round(mainShip[field].replace(',','') * 100);
+        let delta = null;
+        if(!Number.isNaN(shipValue) && !Number.isNaN(mainShipValue)) {
+            delta = shipValue - mainShipValue;
+            if(delta === 0) {
+                return '';
+            }
+            if(delta > 0) {
+                return (
+                    <span style={{color:'green'}}>
+                        {`(+${delta/100})`}
+                    </span>
+                )
+            }
+            if(delta < 0) {
+                return (
+                    <span style={{color:'red'}}>
+                        {`(${delta/100})`}
+                    </span>
+                )
+            }
+        } else {
+            return '';
+        }
+    }
+    
     return (
         <Wrapper>
             <Title>{ship.name}</Title>
-            <Text>Грузоподъемность {ship.cargo_capacity}</Text>
-            <Text>Стоимость {ship.cost_in_credits}</Text>
-            <Text>Экипаж {ship.crew}</Text>
-            <Text>Рейтинг гипердвигателя {ship.hyperdrive_rating}</Text>
-            <Text>Длина {ship.length}</Text>
-            <Text>Скорость в атмосфере {ship.max_atmosphering_speed}</Text>
-            <Text>Пассажиры {ship.passengers}</Text>
+            {
+                Object.keys(fields).map(field => (<Text key={field}>
+                    <div style={{fontSize: '1em'}}>{fields[field]}</div>
+                    {ship[field]+' '}
+                    {secondaryFlag && compare(ship, mainShip, field)}
+                </Text>))
+            }
         </Wrapper>
     )
 }
@@ -37,6 +106,8 @@ const Text = styled.div`
     margin-top: 15px;
     display: flex;
     justify-content: space-between;
+    min-height: 70px;
+    border: 1px solid #eee;
 `
 
 export default CardShip
