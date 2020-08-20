b832cf97cc3f3399361feaceb208ecd9e3e45f14
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-21 02:09:19 +0300

    Update styles.js

--- src/base/styles.js
+++ src/base/styles.js
@@ -1,9 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 import breakpoint from 'styled-components-breakpoint'
+import { GlobalFonts } from './fonts/fonts'
 
 export const GlobalStyle = createGlobalStyle`
+
+	${GlobalFonts}
+
 	body {	
-		font-family: 'Open Sans';
+		font-family: 'Roboto', sans-serif;
 		padding: 20px;
 		${breakpoint('xs', 'sm')`
 			padding: 5px;
