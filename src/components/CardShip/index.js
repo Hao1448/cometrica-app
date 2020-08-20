b3597fdaa95cfd1f25c4a702a5c55e9db0241e8a
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-20 22:23:18 +0300

    update CardShip

--- src/components/CardShip/index.js
+++ src/components/CardShip/index.js
@@ -2,34 +2,6 @@ import React from 'react';
 import styled from 'styled-components'
 import { h2, p } from 'base/mixins/text'
 
-// const compare = (ship, mainShip, field) => {
-//     const shipValue = Math.round(ship[field].replace(',','') * 100);
-//     const mainShipValue = Math.round(mainShip[field].replace(',','') * 100);
-//     let delta = null;
-//     if(!Number.isNaN(shipValue) && !Number.isNaN(mainShipValue)) {
-//         delta = shipValue - mainShipValue;
-//         if(delta === 0) {
-//             return '';
-//         }
-//         if(delta > 0) {
-//             return (
-//                 <span style={{color:'green'}}>
-//                     {`(+${delta/100})`}
-//                 </span>
-//             )
-//         }
-//         if(delta < 0) {
-//             return (
-//                 <span style={{color:'red'}}>
-//                     {`(${delta/100})`}
-//                 </span>
-//             )
-//         }
-//     } else {
-//         return '';
-//     }
-// }
-
 const CardShip = ({ship, mode, mainShip}) => {
 
     const secondaryFlag = (mode === 'secondary')
@@ -71,16 +43,20 @@ const CardShip = ({ship, mode, mainShip}) => {
             return '';
         }
     }
-    
+
     return (
         <Wrapper>
             <Title>{ship.name}</Title>
             {
-                Object.keys(fields).map(field => (<Text key={field}>
-                    <div style={{fontSize: '1em'}}>{fields[field]}</div>
-                    {ship[field]+' '}
-                    {secondaryFlag && compare(ship, mainShip, field)}
-                </Text>))
+                Object.keys(fields).map(field => (
+                    <Text key={field}>
+                        <Name style={{fontSize: '1em'}}>{fields[field]}</Name>
+                        <Feature>
+                            {ship[field]+' '}
+                            {secondaryFlag && compare(ship, mainShip, field)}
+                        </Feature>
+                    </Text>
+                    ))
             }
         </Wrapper>
     )
@@ -91,10 +67,10 @@ const Wrapper = styled.div`
     cursor: pointer;
     border-radius: 20px;
     padding: 15px;
-    transition: box-shadow 0.2s;
-    border: 2px solid ${p => p.theme.color.primary};
+    transition: box-shadow .2s;
+    border: 2px solid ${p => p.theme.color.grey};
     &:hover {
-        box-shadow: 0 0 5px 5px  ${p => p.theme.color.primary};
+        box-shadow: 0 0 5px 5px  ${p => p.theme.color.blue};
     }
 `
 
@@ -106,8 +82,11 @@ const Text = styled.div`
     margin-top: 15px;
     display: flex;
     justify-content: space-between;
-    min-height: 70px;
-    border: 1px solid #eee;
 `
+const Name = styled.div`
 
+`
+const Feature = styled.div`
+    text-align: right;
+`
 export default CardShip
