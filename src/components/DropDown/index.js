689eab1012c20ca814ff0ba1406303c8ae5c810d
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-20 22:23:26 +0300

    Update DropDown

---
+++ src/components/DropDown/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import { h2 } from 'base/mixins/text'
+
+const DropDown = ({ships = [], onChange}) => {
+    return (
+        <Wrapper onChange={onChange}>
+            {
+                ships.map(ship => (
+                    <option
+                        key={ship.name}
+                        value={ship.name}
+                    >
+                        {ship.name}
+                    </option>))
+            }
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.select`
+    ${h2};
+    width: 100%;
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+    border: 2px solid grey;
+    transition: border-color .2s;
+    &:hover, &:active {
+        border: 2px solid ${p => p.theme.color.blue};
+    }
+    &:focus {
+        outline: none;
+    }
+`
+export default DropDown
