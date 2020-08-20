ef5f6faedb6f7efa0802cab33cf649f713525587
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-20 22:23:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import styled, { ThemeProvider} from 'styled-components'
 import { GlobalTheme } from 'base/theme'
 import { GlobalStyle } from 'base/styles'
 import { WidgetCards } from 'components'
+import { h1} from 'base/mixins/text'
 
-function App() {
-  return (
-    <ThemeProvider theme={GlobalTheme}>
-      <GlobalStyle/>
-        <Wrapper>
-          <WidgetCards/>
-        </Wrapper>
-    </ThemeProvider>
-  );
+export default function App() {
+    return (
+        <ThemeProvider theme={GlobalTheme}>
+            <GlobalStyle/>
+            <Wrapper>
+                <Title>Cometrica App</Title>
+                <WidgetCards/>
+            </Wrapper>    
+        </ThemeProvider>
+    )
 }
 
-const Wrapper = styled.div` 
-  padding-top: 20px;
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
-export default App;
+const Title = styled.div`
+    ${h1};
+`
